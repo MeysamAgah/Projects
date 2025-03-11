47a5d803ec912707ca3d7e747da75175e13cadc6
Meysam Agah <113939902+MeysamAgah@users.noreply.github.com>
2025-03-11 21:29:09 +0330

    Update related_games.py

--- Elden_Ring_game_analysis/related_games.py
+++ Elden_Ring_game_analysis/related_games.py
@@ -2,6 +2,18 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
+def web_driver(user_agent_string='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'):
+    options = webdriver.ChromeOptions()
+    options.add_argument("--verbose")
+    options.add_argument('--no-sandbox')
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument("--window-size=1920, 1200")
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument(f"user-agent={user_agent_string}")
+    driver = webdriver.Chrome(options=options)
+    return driver
+
 def related_games(game):
   """
   input:
@@ -19,9 +31,9 @@ def related_games(game):
       related_games.append(e.get_attribute('href').split('/')[-2])
     except:
       print('failed to get a related game name')
-  
+
   driver.quit()
-  
+
   return related_games
 
 def game_platforms(game):
@@ -41,7 +53,7 @@ def game_platforms(game):
     platform = e.get_attribute('href').split('platform=')[-1]
     if platform not in platforms:
       platforms.append(platform)
-  
+
   driver.quit()
-  
+
   return platforms
